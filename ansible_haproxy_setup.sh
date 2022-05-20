cb64993b0fb475233b59cbd06e292b6c9ced0b7a
Tikijavi <74424481+Tikijavi@users.noreply.github.com>
2022-05-20 15:01:41 +0200

    Update ansible_haproxy_setup.sh

--- ansible_haproxy_setup.sh
+++ ansible_haproxy_setup.sh
@@ -5,6 +5,7 @@ sudo apt install ansible -y
 sudo mkdir -p /etc/ansible/roles/haproxy/files/
 sudo mkdir -p /etc/ansible/roles/haproxy/tasks/
 sudo mkdir -p /etc/ansible/roles/apache/tasks/
+sudo mkdir -p /etc/ansible/roles/apache/files/
 wget https://raw.githubusercontent.com/Tikijavi/HAproxy_Loadbalancing/main/apache.yml
 wget https://raw.githubusercontent.com/Tikijavi/HAproxy_Loadbalancing/main/haproxy.cfg
 wget https://raw.githubusercontent.com/Tikijavi/HAproxy_Loadbalancing/main/haproxy.yml
